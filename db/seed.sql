e458b565f3d040b37f2420c7c6ab2562be6e4ab9
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-06-05 15:30:52 +0100

    changes

--- db/seed.sql
+++ db/seed.sql
@@ -15,11 +15,11 @@ VALUES
 (5,"Legal Team Lead", 250000, 4);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1,"Maryama", "Gure", 1, 3),
-(2,"Hajara", "Bile", 2, 1),
-(3,"Kaltuun", "Abdi", 3, null),
-(4,"Cowther", "Sardheye", 4, 3),
-(5,"Asiya", "Duale", 5, null),
-(6,"Najma", "Jama", 2, null),
-(7,"Maymuna", "Abdullahi", 4, 7),
-(8,"Hawa", "Haji", 1, 2);
+VALUES (1,"Harry", "Potter", 1, 1),
+(2,"Hermoine", "Granger", 2, 2),
+(3,"Ron", "Weasley", 3, null),
+(4,"Lord", "Voldemort", 4, 4),
+(5,"Draco", "Malfoy", 5, null),
+(6,"Neville", "Longbottom", 6, null),
+(7,"Rubeus", "Hagrid", 7, 7),
+(8,"Luna", "Lovegood", 8, 8);
