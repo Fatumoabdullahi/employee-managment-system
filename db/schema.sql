6279581f2ee852fa97408702e0b20fa6971df5db
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-06-09 20:09:43 +0100

    done

--- db/schema.sql
+++ db/schema.sql
@@ -9,6 +9,15 @@ CREATE TABLE department (
   name VARCHAR(30)  NOT NULL
 );
 
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY(department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
--- db/seed.sql
+++ db/seed.sql
@@ -15,11 +15,11 @@ VALUES
 (5,"Legal Team Lead", 250000, 4);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1,"Harry", "Potter", 1, 1),
-(2,"Hermoine", "Granger", 2, 2),
+VALUES (1,"Harry", "Potter", 1, 3),
+(2,"Hermoine", "Granger", 2, 3),
 (3,"Ron", "Weasley", 3, null),
-(4,"Lord", "Voldemort", 4, 4),
+(4,"Lord", "Voldemort", 4, 5),
 (5,"Draco", "Malfoy", 5, null),
-(6,"Neville", "Longbottom", 6, null),
-(7,"Rubeus", "Hagrid", 7, 7),
-(8,"Luna", "Lovegood", 8, 8);
+(6,"Neville", "Longbottom", 5, null),
+(7,"Rubeus", "Hagrid", 5, 3),
+(8,"Luna", "Lovegood", 5, 2);
